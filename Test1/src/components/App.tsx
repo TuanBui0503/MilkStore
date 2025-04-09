bc04932c504d6611c38f12ffebdc495c4d04405a
TuanBui0503 <buiquoctuan532001@gmail.com>
2025-04-09 09:07:12 +0700

    sửa

--- Test1/src/components/App.tsx
+++ Test1/src/components/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
     <div className="container mx-auto  px-10">
+      <h1 className="text-center text-5xl mt-4 font-bold">Milk Store</h1>
     <SortFilter />
     </div>
     </>
--- Test1/src/components/SortFilter.tsx
+++ Test1/src/components/SortFilter.tsx
@@ -1,73 +1,73 @@
-import { useState } from "react"
-import { milkProducts } from "./data"
-import SanPham from "./SanPham"
-import Store from "./Store"
+import { useState } from 'react'
+import { milkProducts } from './data'
+import SanPham from './SanPham'
+import Store from './Store'
 
 export default function SortFilter() {
-    const [sort, setSort] = useState("default")
-    const [filter, setFilter] = useState("all")
-    const [newList, setNewList] = useState(milkProducts)
-    const [store, setStore] = useState([])
+  const [sort, setSort] = useState('default')
+  const [filter, setFilter] = useState('all')
+  const [newList, setNewList] = useState(milkProducts)
+  const [store, setStore] = useState([])
 
-    const handleFilter = (value: string) => {
-        setFilter(value)
-        if (value === "all") {
-            setNewList(milkProducts)
-        } else {
-            const productFilter = milkProducts.filter((i) => i.loại === value)
-            setNewList(productFilter)
-        }
+  const handleFilter = (value: string) => {
+    setFilter(value)
+    if (value === 'all') {
+      setNewList(milkProducts)
+    } else {
+      const productFilter = milkProducts.filter((i) => i.loại === value)
+      setNewList(productFilter)
     }
+  }
 
-    const handleSort = (value: string) => {
-        setSort(value)
-        let sortList = [...newList]
-        if (value === "hight") {
-            sortList.sort((a, b) => b.price - a.price)
-        }  
-        if (value === "low") {
-            sortList.sort((a, b) => a.price - b.price)
-        }
-        setNewList(sortList)
+  const handleSort = (value: string) => {
+    setSort(value)
+    let sortList = [...newList]
+    if (value === 'hight') {
+      sortList.sort((a, b) => b.price - a.price)
     }
+    if (value === 'low') {
+      sortList.sort((a, b) => a.price - b.price)
+    }
+    setNewList(sortList)
+  }
 
-    const addProduct = (product) => {
-        setStore(prev => [...prev, product])
-    }
-    
-    return (
-        <>
-        <Store store={store} />
-            <div className="flex justify-between py-10">
-                <label htmlFor="">
-                    Loại sữa:
-                    <select 
-                        onChange={(e) => handleFilter(e.target.value)}
-                        className="border border-solid border-black mx-2 py-1 rounded-xl"
-                    >
-                        <option value="all">Tất cả</option>
-                        <option value="sữa chua">sữa chua</option>
-                        <option value="sữa tươi">sữa tươi</option>
-                        <option value="sữa đặc">sữa đặc</option>
-                        <option value="sữa đậu nành">sữa đậu nành</option>
-                        <option value="sữa chua uống">sữa chua uống</option>
-                        <option value="sữa bắp">sữa bắp</option>
-                        <option value="sữa hạt">sữa hạt</option>
-                    </select>
-                </label>
-                <label htmlFor="">
-                    Sắp xếp:
-                    <select 
-                        onChange={(e) => handleSort(e.target.value)}
-                        className="border border-solid border-black mx-2 py-1 rounded-xl"
-                    >
-                        <option value="default">Mặc định</option>
-                        <option value="hight">Theo giá từ cao đến thấp</option>
-                        <option value="low">Theo giá từ thấp đến cao</option>
-                    </select>
-                </label>
-            </div>
-            <SanPham newList={newList}  addProduct={addProduct} />
-        </>
-    )
+  const addProduct = (product) => {
+    setStore((store) => [...store, product])
+  }
+
+  return (
+    <>
+      <Store store={store} />
+      <div className="flex justify-between py-10">
+        <label htmlFor="">
+          Loại sữa:
+          <select
+            onChange={(e) => handleFilter(e.target.value)}
+            className="border border-solid border-black mx-2 py-1 rounded-xl"
+          >
+            <option value="all">Tất cả</option>
+            <option value="sữa chua">sữa chua</option>
+            <option value="sữa tươi">sữa tươi</option>
+            <option value="sữa đặc">sữa đặc</option>
+            <option value="sữa đậu nành">sữa đậu nành</option>
+            <option value="sữa chua uống">sữa chua uống</option>
+            <option value="sữa bắp">sữa bắp</option>
+            <option value="sữa hạt">sữa hạt</option>
+          </select>
+        </label>
+        <label htmlFor="">
+          Sắp xếp:
+          <select
+            onChange={(e) => handleSort(e.target.value)}
+            className="border border-solid border-black mx-2 py-1 rounded-xl"
+          >
+            <option value="default">Mặc định</option>
+            <option value="hight">Theo giá từ cao đến thấp</option>
+            <option value="low">Theo giá từ thấp đến cao</option>
+          </select>
+        </label>
+      </div>
+      <SanPham newList={newList} addProduct={addProduct} />
+    </>
+  )
 }
